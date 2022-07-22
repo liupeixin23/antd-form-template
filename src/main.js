a1c3dd472ea9500c5a68f06c37970281d193619d
liupeixin <liupx@nancal.com>
2022-07-22 10:34:08 +0800

    fix:优化

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: liupeixin liupx@nancal.com
  * @Date: 2022-05-20 15:16:15
  * @LastEditors: liupeixin liupx@nancal.com
- * @LastEditTime: 2022-07-22 10:30:15
+ * @LastEditTime: 2022-07-22 10:33:45
  * @FilePath: /antd-form-template/src/main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,8 +11,6 @@ import App from './App.vue';
 import router from '@/permission.js';
 import store from './store';
 import Antd from 'ant-design-vue';
-
-// import { Form, Button, Cascader, Input, InputNumber, Select } from 'ant-design-vue'
 
 import 'ant-design-vue/dist/antd.css';
 
@@ -30,7 +28,6 @@ dayjs.locale('zh-cn');
 import util from '@/utils/util.js'; // 工具类
 
 import '@/utils/directive.js';
-// Vue.use(Form).use(Button).use(Cascader).use(Input).use(InputNumber).use(Select)
 Vue.use(Antd);
 Vue.use(ElementUI, { size: 'small' })
 Vue.use(util); // 工具类
